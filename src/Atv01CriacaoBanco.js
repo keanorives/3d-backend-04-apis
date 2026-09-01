857e1833c8f554fd6f59b01364ed05a578c00ec5
jhon marciano <thiago.maia@nave.org.br>
2026-09-01 10:11:42 -0300

    Update Atv01CriacaoBanco.js

--- src/Atv01CriacaoBanco.js
+++ src/Atv01CriacaoBanco.js
@@ -24,3 +24,33 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+const db = new sqlite3.Database("./beyblade.db", (erro) => {
+  if (erro) {
+    console.error(erro.message);
+  } else {
+    console.log("Banco criado com sucesso!");
+  }
+});
+db.run(`
+  CREATE TABLE IF NOT EXISTS beyblades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )
+`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "indexAtv.html"));
+});
+app.listen(3000);
+module.exports = app;
