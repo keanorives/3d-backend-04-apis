2380e6d93b6900abd5542e84ef88463c9d675260
jhon marciano <thiago.maia@nave.org.br>
2026-09-01 10:13:08 -0300

    Update Atv04EliminarItem.js

--- src/Atv04EliminarItem.js
+++ src/Atv04EliminarItem.js
@@ -16,3 +16,62 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+const db = new sqlite3.Database("./beyblade.db");
+db.run(`
+  CREATE TABLE IF NOT EXISTS beyblades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL UNIQUE,
+    lamina TEXT,
+    catraca TEXT,
+    ponta TEXT,
+    participante TEXT NOT NULL UNIQUE
+  )
+`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "indexAtv.html"));
+});
+app.get("/api/beyblade", (req, res) => {
+  db.all("SELECT * FROM beyblades", [], (erro, linhas) => {
+    if (erro) {
+      return res.status(400).json({ error: erro.message });
+    }
+    res.status(200).json({
+      message: "Sucesso",
+      data: linhas,
+    });
+  });
+});
+app.get("/api/beyblade/cadastrar", (req, res) => {
+  const { nome, lamina, catraca, ponta, participante } = req.query;
+  db.run(
+    `INSERT INTO beyblades (nome, lamina, catraca, ponta, participante)
+     VALUES (?, ?, ?, ?, ?)`,
+    [nome, lamina, catraca, ponta, participante]
+  );
+  res.json({ message: "Beyblade cadastrada com sucesso!" });
+});
+app.delete("/api/beyblade/remover/:id", (req, res) => {
+  const id = req.params.id;
+  db.run(
+    "DELETE FROM beyblades WHERE id = ?",
+    [id],
+    (erro) => {
+      if (erro) {
+        return res.status(400).json({ error: erro.message });
+      }
+      res.status(200).json({
+        message: "Beyblade removida com sucesso!",
+      });
+    }
+  );
+});
+app.listen(3000);
+module.exports = app;
